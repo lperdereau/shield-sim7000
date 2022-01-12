c2fe5e40a686ef59481810b5f47b142d4b9e47e1
Louis PERDEREAU <leolouis.perdereau@gmail.com>
2022-01-13 00:50:17 +0100

    fix: Correct Send SMS function

--- src/clients/sms/at_commands.rs
+++ src/clients/sms/at_commands.rs
@@ -1,2 +1,4 @@
+pub(super) const MAX_SEND_SMS_SIZE: usize = 160;
+
 pub(super) const CMGF: &str = "+CMGF"; // Select SMS Format (0 or 1)
 pub(super) const CMGS: &str = "+CMGS"; // Send SMS
--- src/clients/sms/mod.rs
+++ src/clients/sms/mod.rs
@@ -2,7 +2,7 @@ mod at_commands;
 
 use std::{thread, time::Duration};
 
-use self::at_commands::{CMGF, CMGS};
+use self::at_commands::{CMGF, CMGS, MAX_SEND_SMS_SIZE};
 use crate::serial::SerialClient;
 use at_commands_crate::builder::CommandBuilder;
 use log::error;
@@ -27,28 +27,38 @@ impl SMSClient {
     }
 
     pub fn send_sms(&mut self, message: &str, number: &str) -> bool {
-        if message.len() > 998 {
-            error!("Message is too long ({} chars), max is 998", message.len());
-            return false;
-        }
         let mut buffer = [0; 1000];
-        CommandBuilder::create_set(&mut buffer, true)
-            .named(CMGS)
-            .with_string_parameter(number);
-        if !self.client.send(&buffer) {
+
+        let first_end: usize = 12 + number.len();
+        if message.len() > MAX_SEND_SMS_SIZE {
+            error!(
+                "Message is too long ({} chars), max is {}",
+                message.len(),
+                MAX_SEND_SMS_SIZE
+            );
             return false;
         }
 
-        CommandBuilder::create_execute(&mut buffer, false)
-            .named(message)
+        CommandBuilder::create_set(&mut buffer[..first_end], true)
+            .named(CMGS)
+            .with_string_parameter(number)
             .finish()
             .unwrap();
 
-        if !self.client.send(&buffer) {
+        if !self.client.send(&buffer[..first_end]) {
             return false;
         }
-        thread::sleep(Duration::from_millis(4000));
-        self.client.read_incoming_raw_data();
+
+        thread::sleep(Duration::from_millis(1000));
+        let second_end: usize = first_end + MAX_SEND_SMS_SIZE + 1;
+        CommandBuilder::create_execute(&mut buffer[first_end..second_end], false)
+            .named(&message[..MAX_SEND_SMS_SIZE])
+            .finish_with(b"\x1a")
+            .unwrap();
+
+        if !self.client.send(&buffer[first_end..second_end]) {
+            return false;
+        }
         true
     }
 }
